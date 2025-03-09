2c249bfdfd04086d4027d5ddda50cef7c810014f
David Zaitsev <zaitsevdavid4@gmail.com>
2025-03-09 19:30:03 +0200

    update gitignore

---
+++ Plugin.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+
+namespace lethalCompanyMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class SomeMod : BaseUnityPlugin
+    {
+        private const string modGUID = "Somemod.ByMe";
+        private const string modName = "Somemod";
+        private const string modVersion = "1.0.0";
+    }
+}
